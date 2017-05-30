0a482c27abc631b75154f5b8a94cea33a8d6c7f2
Michael Fiano <michael.fiano@gmail.com>
2017-05-30 04:10:42 -0400

    Update

--- t/test-vector.lisp
+++ t/test-vector.lisp
@@ -8,13 +8,17 @@
 (diag "Running vector tests.")
 
 (subtest "Vector creation: (vec 1 2 3)"
-  (plan 5)
+  (plan nil)
+  (is-type (vec 1 2 3) '(simple-array single-float (3))
+           "Vector is a Common Lisp vector of 3 single-floats")
+  (is (vref (vec 1 2 3) 0) 1.0
+      "Vector virtualized reference of X component")
+  (is (vref (vec 1 2 3) 1) 2.0
+      "Vector virtualized reference of Y component")
+  (is (vref (vec 1 2 3) 2) 3.0
+      "Vector virtualized reference of Z component")
   (with-vector (v (vec 1 2 3))
-    (is-type v '(simple-array single-float (3))
-        "Vector type is a Common Lisp vector of 3 single-floats")
-    (is v #(1 2 3) :test #'equalp
-        "Vector is equivalent to #(1 2 3)")
-    (is vx 1 "Component X is 1.0")
-    (is vy 2.0 "Component Y is 2.0")
-    (is vz 3.0 "Component Z is 3.0"))
+    (is vx 1.0 "with-vector reading of X component")
+    (is vy 2 "with-vector reading of Y component")
+    (is vz 3.0 "with-vector reading of Z component"))
   (finalize))
